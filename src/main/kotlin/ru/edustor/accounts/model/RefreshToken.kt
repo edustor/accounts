052811c73eba4313894008114c30c36969f55650
Dmitry Romanov <me@wutiarn.ru>
2016-10-08 20:41:28 +0300

    Preserve scope on token refresh

--- src/main/kotlin/ru/edustor/accounts/model/RefreshToken.kt
+++ src/main/kotlin/ru/edustor/accounts/model/RefreshToken.kt
@@ -3,6 +3,7 @@ package ru.edustor.accounts.model
 import org.hibernate.annotations.OnDelete
 import org.hibernate.annotations.OnDeleteAction
 import ru.edustor.accounts.util.genRandomToken
+import java.time.Instant
 import java.util.*
 import javax.persistence.CascadeType
 import javax.persistence.Entity
@@ -16,12 +17,18 @@ class RefreshToken() {
     var id: String = UUID.randomUUID().toString()
 
     lateinit var token: String
+    lateinit var scope: String
+    lateinit var lastUsed: Instant
+
+    //todo: Cascade updates
     @OneToOne(cascade = arrayOf(CascadeType.REMOVE))
     @OnDelete(action = OnDeleteAction.CASCADE)
     lateinit var account: Account
 
-    constructor(account: Account) : this() {
+    constructor(account: Account, scope: String) : this() {
         this.account = account
         this.token = genRandomToken()
+        this.scope = scope
+        lastUsed = Instant.now()
     }
 }
--- src/main/kotlin/ru/edustor/accounts/oauth2/rest/TokenController.kt
+++ src/main/kotlin/ru/edustor/accounts/oauth2/rest/TokenController.kt
@@ -2,6 +2,7 @@ package ru.edustor.accounts.oauth2.rest
 
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
@@ -20,12 +21,15 @@ import java.io.File
 import java.security.KeyFactory
 import java.security.PrivateKey
 import java.security.spec.PKCS8EncodedKeySpec
+import java.time.Instant
 import java.util.*
 
 @RestController
 @RequestMapping(value = "/oauth2/token", method = arrayOf(RequestMethod.POST))
 class TokenController(val googleProvider: GoogleProvider, val refreshTokenRepository: RefreshTokenRepository) {
-    val TOKEN_EXPIRE_IN = 60 * 60 // Seconds
+    val logger = LoggerFactory.getLogger(TokenController::class.java)
+
+    val TOKEN_EXPIRE_IN = 1 * 60 // Seconds
 
     val signkey: PrivateKey
 
@@ -38,11 +42,8 @@ class TokenController(val googleProvider: GoogleProvider, val refreshTokenReposi
     @RequestMapping
     fun token(@RequestParam payload: Map<String, String>): Map<String, Any> {
         val grantType = payload["grant_type"] ?: throw MissingArgumentException("grant_type")
-        val scope = payload["scope"] ?: ""
 
-        val scopeList = scope.split(" ")
-
-        val account = when (grantType) {
+        val (account, scope) = when (grantType) {
             "password" -> processPasswordGrant(payload)
             "refresh_token" -> processRefreshTokenGrant(payload)
             else -> throw OAuthException("unsupported_grant_type", "$grantType is not recognized")
@@ -56,16 +57,21 @@ class TokenController(val googleProvider: GoogleProvider, val refreshTokenReposi
                 "scope" to scope
         )
 
-        if ("offline" in scopeList) {
-            val rt = RefreshToken(account)
+        val scopeList = scope.split(" ")
+        if ("offline" in scopeList && grantType != "refresh_token") {
+            val rt = RefreshToken(account, scope)
             refreshTokenRepository.save(rt)
             resp["refresh_token"] = rt.token
         }
 
+        logger.info("Issuing token for ${account.id} with grant type $grantType and scope \"$scope\"")
+
         return resp
     }
 
-    fun processPasswordGrant(payload: Map<String, String>): Account {
+    fun processPasswordGrant(payload: Map<String, String>): Pair<Account, String> {
+        val scope = payload["scope"] ?: ""
+
         val username = payload["username"] ?: throw MissingArgumentException("username")
         val password = payload["password"] ?: throw MissingArgumentException("password")
 
@@ -74,13 +80,15 @@ class TokenController(val googleProvider: GoogleProvider, val refreshTokenReposi
             else -> throw InvalidGrantException("Unknown username")
         }
 
-        return account
+        return account to scope
     }
 
-    fun processRefreshTokenGrant(payload: Map<String, String>): Account {
+    fun processRefreshTokenGrant(payload: Map<String, String>): Pair<Account, String> {
         val tokenStr = payload["refresh_token"] ?: throw MissingArgumentException("refresh_token")
         val token = refreshTokenRepository.findByToken(tokenStr) ?: throw InvalidGrantException("Invalid or expired refresh token")
-        return token.account
+        token.lastUsed = Instant.now()
+        refreshTokenRepository.save(token)
+        return token.account to token.scope
     }
 
     fun makeToken(account: Account, scope: String): String {
