e4b7a3f45c3ee6f6da7231d5d9d10d604ea2f009
Dmitry Romanov <me@wutiarn.ru>
2016-12-18 00:04:34 +0300

    init migration impl

--- src/main/kotlin/ru/edustor/accounts/migration/V001__Init.kt
+++ src/main/kotlin/ru/edustor/accounts/migration/V001__Init.kt
@@ -1,39 +1,30 @@
 package ru.edustor.accounts.migration
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.github.mongobee.changeset.ChangeLog
 import com.github.mongobee.changeset.ChangeSet
-import com.mongodb.BasicDBObject
-import com.mongodb.DB
+import com.mongodb.*
+import com.mongodb.util.JSON
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.io.File
 
 @ChangeLog(order = "001")
 class V001__Init {
 
-    val logger = LoggerFactory.getLogger(V001__Init::class.java)
+    val logger: Logger = LoggerFactory.getLogger(V001__Init::class.java)
 
     @ChangeSet(author = "wutiarn", id = "001_init", order = "001")
     fun init(db: DB) {
-        val jsonFile = File("migrations/001.json")
+        val jsonFile = File("migrations/001.json") // mongo-compatible arrray
         if (!jsonFile.exists()) {
             logger.warn("migrations/001.json not found. Skipping migration.")
         }
 
-
-        val objectMapper = ObjectMapper()
-        val accounts = objectMapper.readTree(jsonFile)
-//        val accounts = objectMapper.readValue<List<Account>>(jsonFile, object : TypeReference<List<Account>>() {})
+        val basicDBList = JSON.parse(jsonFile.readText()) as BasicDBList
 
         val collection = db.getCollection("account")
-        val bsonList = accounts.map { account ->
-            val bsonObject = BasicDBObject()
-            account.fieldNames().forEach { key ->
-                bsonObject.put(key, account[key].textValue())
-            }
-            return@map bsonObject
+        basicDBList.forEach {
+            collection.save(it as DBObject)
         }
-
-        collection.insert(bsonList)
     }
 }
