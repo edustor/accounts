5db82600946060dabd9b3973621f0135a4577fcb
Dmitry Romanov <me@wutiarn.ru>
2016-12-24 17:36:55 +0300

    Version controller

--- src/main/kotlin/ru/edustor/accounts/controller/RootController.kt
+++ src/main/kotlin/ru/edustor/accounts/controller/RootController.kt
@@ -3,14 +3,14 @@ package ru.edustor.accounts.controller
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
-import ru.edustor.accounts.util.EdustorVersionInfoHolder
+import ru.edustor.commons.version.EdustorVersionInfoHolder
 
 @Controller
-class RootController(val versionHolder: EdustorVersionInfoHolder) {
+class RootController(val versionInfoHolder: EdustorVersionInfoHolder) {
     @RequestMapping("/")
     @ResponseBody
     fun root(): String {
-        return "Welcome to Edustor Accounts Microservice v${versionHolder.version}. <br><br> " +
+        return "Welcome to Edustor Accounts Microservice v${versionInfoHolder.version}. <br><br> " +
                 "There is no human interface so far. If you're developer, please refer to Edustor API Documentation"
     }
 }
