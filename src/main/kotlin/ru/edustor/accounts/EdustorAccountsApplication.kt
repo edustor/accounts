ef762638040e409073cc09a148247e9c8651b27f
Dmitry Romanov <me@wutiarn.ru>
2017-05-02 18:31:30 +0300

    Allow CORS

--- src/main/kotlin/ru/edustor/accounts/EdustorAccountsApplication.kt
+++ src/main/kotlin/ru/edustor/accounts/EdustorAccountsApplication.kt
@@ -1,6 +1,12 @@
 package ru.edustor.accounts
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @SpringBootApplication
-open class EdustorAccountsApplication
+open class EdustorAccountsApplication : WebMvcConfigurerAdapter() {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**").allowedOrigins("*")
+    }
+}
